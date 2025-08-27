aa1ad5313c7fbde124b9e9416fc38b2efdeddd58
billdeptrai0512 <billnguyen05121998@gmail.com>
2025-08-27 22:13:30 +0700

    handle error nearby

--- src/map/nearby.jsx
+++ src/map/nearby.jsx
@@ -4,7 +4,6 @@ import styles from "./map.module.css";
 
 export default function NearByButton() {
   const { saveLocation } = useUserLocation();
-  const [error, setError] = useState(null);
   const [permissionState, setPermissionState] = useState("prompt"); // prompt | granted | denied
 
   useEffect(() => {
@@ -19,31 +18,25 @@ export default function NearByButton() {
   }, []);
 
   const handleRequestLocation = () => {
-    setError(null);
 
-    if (!navigator.geolocation) {
-      setError({ message: "Trình duyệt của bạn không hỗ trợ định vị." });
-      return;
+    if (permissionState === "denied") {
+      return alert("Bạn đã từ chối quyền truy cập vị trí trước đó vì vậy vui lòng bật lại trong cài đặt trình duyệt.")
     }
-
+  
+    if (!navigator.geolocation) {
+      return alert("Trình duyệt của bạn không hỗ trợ định vị.")
+    }
+  
     navigator.geolocation.getCurrentPosition(
       (pos) => {
         const { latitude, longitude } = pos.coords;
-        console.log("SUCCESS:", latitude, longitude);
-
-        // save to context/store
         saveLocation({ lat: latitude, lng: longitude });
-
       },
       (err) => {
-        console.error("ERROR:", err);
         if (err.code === 1) {
-          setError({
-            message:
-              "Chúng tôi cần quyền truy cập vị trí để cho bạn trải nghiệm tốt nhất.",
-          });
-        } else {
-          setError({ message: "Không thể lấy vị trí của bạn." });
+          alert("Tính năng này cần thông tin vị trí của bạn để hoạt động.")
+        } else {;
+          alert("Không thể lấy vị trí của bạn.")
         }
       },
       { enableHighAccuracy: true }
@@ -52,19 +45,6 @@ export default function NearByButton() {
 
   return (
     <div>
-      {error && (
-        <p style={{ color: "red", marginTop: "8px" }}>
-          ⚠ {error.message}
-        </p>
-      )}
-
-      {permissionState === "denied" && (
-        <p style={{ color: "orange", marginTop: "8px" }}>
-          Bạn đã chặn quyền truy cập vị trí. Hãy bật lại trong cài đặt trình
-          duyệt để sử dụng tính năng này.
-        </p>
-      )}
-
       <button className={styles.nearby_button} onClick={handleRequestLocation}>
         <h2 style={{ margin: 0 }}>Staycation gần bạn</h2>
       </button>
